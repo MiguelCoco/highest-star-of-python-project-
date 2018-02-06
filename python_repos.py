4764548905f19c6c3e1e0c7127df237c0f6456c7
miguelcoco <35021229+MiguelCoco@users.noreply.github.com>
2018-02-06 15:14:21 +0800

    Update python_repos.py

--- python_repos.py
+++ python_repos.py
@@ -17,10 +17,16 @@ repo_dicts = response_dict['items']
 
 #print('Repositories returned: ',len(repo_dicts))
 #print("\nSelected information about each repository:")
-names, stars = [], []
+names,plot_dicts = [], []
 for repo_dict in repo_dicts:
 	names.append(repo_dict['name'])
-	stars.append(repo_dict['stargazers_count'])
+
+	plot_dict = {
+		'value':repo_dict['stargazers_count'],
+		'label':str(repo_dict['description']),
+		'xlink':repo_dict['html_url'],
+		}
+	plot_dicts.append(plot_dict)
 
 #可视化
 my_style = LS('#333366',base_style=LCS)
@@ -38,5 +44,5 @@ chart = pygal.Bar(my_config,style=my_style)
 chart.title='Most-Starred Python Projects on GitHub'
 chart.x_labels = names
 
-chart.add('',stars)
+chart.add('',plot_dicts)
 chart.render_to_file('python_repos.svg')
